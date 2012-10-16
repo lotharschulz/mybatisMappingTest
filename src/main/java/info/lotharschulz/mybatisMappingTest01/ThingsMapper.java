cd64a39b5a56450a0bfb9c5a0d339f0ae554f4e5
Lothar Schulz <mail@lothar-schulz.info>
2012-10-16 22:27:06 +0200

    @todo dropped

--- src/main/java/info/lotharschulz/mybatisMappingTest01/ThingsMapper.java
+++ src/main/java/info/lotharschulz/mybatisMappingTest01/ThingsMapper.java
@@ -9,23 +9,6 @@ public interface ThingsMapper {
     final String SELECTALL = "SELECT * FROM things";
     
     @Select(SELECTALL)
-    
-    /**
-     * @todo
-     * 
-     * properties müssen gleich heißen, dann kein @results nötig, 
-     * bei default einstellung
-     * <setting name="autoMappingBehavior" value="PARTIAL"/>
-     * 
-     * wenn nicht default, sondern
-     * <setting name="autoMappingBehavior" value="NONE"/>
-     * 
-     * @results nötig
-     * 
-     * für beide fälle ein branch
-     * 
-     */
-    
     @Results(value = {
         @Result(property="ID", column="ID"),
         @Result(property="column_one", column="column_one"),
